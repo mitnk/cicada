63b075591856f9818de891a09611bd09c1754cb1
Hugo Wang <w@mitnk.com>
2017-03-09 22:46:49 +0800

    added readline

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
+extern crate rustyline;
+
 use std::env;
-use std::io;
-use std::io::Write;
 use std::process::Command;
 
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
+
+
 fn main() {
-    println!("Rust v0.1.0 Tell me what to do!");
+    println!("RUSH v0.1.0 Tell me what to do!");
+    let mut rl = Editor::<()>::new();
     let user = env::var("USER").unwrap();
     loop {
         print!("{}@rush$ ", user);
-        io::stdout().flush().unwrap();
-        let mut cmd = String::new();
-        io::stdin().read_line(&mut cmd)
-            .expect("failed to read line");
-        if cmd.trim() == "exit" {
-            println!("Bye.");
-            break;
-        } else if cmd.trim() == "" {
-            continue;
+        let prompt = format!("{}@rust: ~$ ", user);
+        let cmd = rl.readline(prompt.as_str());
+        match cmd {
+            Ok(line) => {
+                if line.trim() == "exit" {
+                    println!("Bye.");
+                    break;
+                } else if line.trim() == "" {
+                    continue;
+                }
+                rl.add_history_entry(&line);
+                let args : Vec<&str> = line.trim().split(' ').collect();
+                match Command::new(args[0]).args(&(args[1..])).output() {
+                    Ok(output) => {
+                        print!("{}", String::from_utf8(output.stdout).unwrap());
+                    },
+                    Err(e) => {
+                        println!("{:?}", e);
+                    }
+                }
+            },
+            Err(ReadlineError::Interrupted) => {
+                // Ctrl-C
+                continue;
+            },
+            Err(ReadlineError::Eof) => {
+                // Ctrl-D
+                continue;
+            },
+            Err(err) => {
+                println!("RL Error: {:?}", err);
+                continue;
+            }
         }
-        // let args = cmd.trim().split(' ').collect();
-        let args : Vec<&str> = cmd.trim().split(' ').collect();
-        let output = Command::new(args[0])
-            .args(&(args[1..]))
-            .output()
-            .expect("Failed to run Command");
-        print!("{}", String::from_utf8(output.stdout).unwrap());
     }
 }
