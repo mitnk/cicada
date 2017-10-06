5fac888ccc3cef0abc24e2d3bdf1655eddfdbc98
Hugo Wang <w@mitnk.com>
2017-10-06 12:56:20 +0800

    added git hash and date in cinfo

---
+++ src/build.rs
@@ -0,0 +1,16 @@
+extern crate time;
+use std::process::Command;
+
+fn main() {
+    match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
+        Ok(x) => {
+            let git_hash = String::from_utf8_lossy(&x.stdout);
+            println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+        }
+        Err(e) => {
+            println!("cargo:rustc-env=GIT_HASH={:?}", e);
+        }
+    }
+    let tm = time::now();
+    println!("cargo:rustc-env=BUILD_DATE={}", tm.rfc822());
+}
--- src/builtins/cinfo.rs
+++ src/builtins/cinfo.rs
@@ -3,6 +3,8 @@ use os_type;
 pub fn run(_tokens: &Vec<(String, String)>) -> i32 {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
     println!("Cicada Version: {}", VERSION);
+    println!("Git Hash: {}", env!("GIT_HASH"));
+    println!("Built at: {}", env!("BUILD_DATE"));
     let os = os_type::current_platform();
     println!("OS Type: {:?}", os.os_type);
     println!("OS Version: {}", os.version);
