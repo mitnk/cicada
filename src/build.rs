7820cc84ee9bdeee372fe7b947d20470c8adc1c6
Hugo Wang <w@mitnk.com>
2018-02-22 10:41:59 +0800

    update git hash in cinfo

--- src/build.rs
+++ src/build.rs
@@ -2,7 +2,7 @@ extern crate time;
 use std::process::Command;
 
 fn main() {
-    match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
+    match Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output() {
         Ok(x) => {
             let git_hash = String::from_utf8_lossy(&x.stdout);
             println!("cargo:rustc-env=GIT_HASH={}", git_hash);
