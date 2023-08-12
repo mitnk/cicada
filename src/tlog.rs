0e4798279722810a04ab33d28b160b0a10eb4210
Hugo Wang <w@mitnk.com>
2023-08-12 12:59:13 +0800

    upgrade libs

--- src/tlog.rs
+++ src/tlog.rs
@@ -15,20 +15,20 @@ macro_rules! log {
             use std::io::Write as _;
 
             let msg = $fmt;
-            let mut cfile;
             match std::fs::OpenOptions::new().append(true).create(true).open(&log_file) {
-                Ok(x) => cfile = x,
-                Err(_) => panic!("tlog: open file error"),
+                Ok(mut cfile) => {
+                    let pid = crate::tlog::getpid();
+                    let now = crate::ctime::DateTime::now();
+                    let msg = format!("[{}][{}] {}", now, pid, msg);
+                    let msg = if msg.ends_with('\n') { msg } else { format!("{}\n", msg) };
+                    match cfile.write_all(msg.as_bytes()) {
+                        Ok(_) => {}
+                        Err(_) => println!("tlog: write_all error")
+                    }
+                }
+                Err(_) => println!("tlog: open file error"),
             }
 
-            let pid = crate::tlog::getpid();
-            let now = crate::ctime::DateTime::now();
-            let msg = format!("[{}][{}] {}", now, pid, msg);
-            let msg = if msg.ends_with('\n') { msg } else { format!("{}\n", msg) };
-            match cfile.write_all(msg.as_bytes()) {
-                Ok(_) => {}
-                Err(_) => panic!("tlog: write_all error")
-            }
         }
     );
 
