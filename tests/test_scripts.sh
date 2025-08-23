86ef292f7c728d2289b6754ceecd9e8580e77944
Aster Boese <asterboese@mailbox.org>
2025-08-23 14:44:24 -0400

    tests: fix shellcheck and allow using release version for tests
    Signed-off-by: Aster Boese <asterboese@mailbox.org>

--- tests/test_scripts.sh
+++ tests/test_scripts.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-set -e
-CICADA="./target/debug/cicada"
 
-if [ ! -f $CICADA ]; then
+set -e
+
+CICADA="./target/${MODE}/cicada"
+
+if [ ! -f "$CICADA" ]; then
     echo "cicada binary not found: $CICADA"
     echo "please build it out with cargo build first"
     exit 1
@@ -11,10 +13,10 @@ fi
 DIR_TEST="$( cd "$(dirname "$0")" ; pwd -P )"
 TMP_FILE="${DIR_TEST}/tmpfile-test-cicada-scripts.out"
 
-for src in ${DIR_TEST}/scripts/*.sh; do
-    echo Runing $CICADA $src
-    $CICADA $src > "$TMP_FILE"
-    if diff -u "${src}.out" $TMP_FILE; then
+for src in "${DIR_TEST}"/scripts/*.sh; do
+    echo "Running $CICADA $src"
+    CICADA="$CICADA" "$CICADA" "$src" > "$TMP_FILE"
+    if diff -u "${src}.out" "$TMP_FILE"; then
         echo OK.
     else
         echo Failed.
@@ -22,4 +24,4 @@ for src in ${DIR_TEST}/scripts/*.sh; do
     fi
 done
 
-rm -f $TMP_FILE
+rm -f "$TMP_FILE"
