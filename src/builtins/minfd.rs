023c412926c95377f285d9e32745f8a27cef04d5
Hugo Wang <whgking@gmail.com>
2026-01-24 17:42:27 +0800

    remove nix::fs feature

--- src/builtins/minfd.rs
+++ src/builtins/minfd.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::Write;
 use std::os::fd::AsRawFd;
 
@@ -8,14 +9,9 @@ use crate::types::{Command, CommandLine, CommandResult};
 pub fn run(_sh: &mut Shell, cl: &CommandLine, cmd: &Command, capture: bool) -> CommandResult {
     let mut cr = CommandResult::new();
 
-    let fd = nix::fcntl::open(
-        "/dev/null",
-        nix::fcntl::OFlag::empty(),
-        nix::sys::stat::Mode::empty(),
-    );
-    match fd {
-        Ok(fd) => {
-            let info = format!("{}", fd.as_raw_fd());
+    match File::open("/dev/null") {
+        Ok(f) => {
+            let info = format!("{}", f.as_raw_fd());
             print_stdout_with_capture(&info, &mut cr, cl, cmd, capture);
         }
         Err(e) => {
